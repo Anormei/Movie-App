989d78f79cc2007b3e87d7df352a1d5089ff7885
Steven Warlow <steven.warlow@mkodo.com>
2021-10-12 17:20:19 +0100

    [RM-85416] Added map and stored loaded movies in a map

--- app/src/main/java/com/mkodo/training/stevenw/movieapp/api/MovieRepositoryImpl.kt
+++ app/src/main/java/com/mkodo/training/stevenw/movieapp/api/MovieRepositoryImpl.kt
@@ -9,6 +9,7 @@ import kotlinx.coroutines.withContext
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+
 //TODO make it a singleton
 object MovieRepositoryImpl : MovieRepository {
 
@@ -16,10 +17,21 @@ object MovieRepositoryImpl : MovieRepository {
 
     //TODO local list of movies
 
+    val map = mutableMapOf<String, Movie>()
+
     override suspend fun getMovie(id: String) =
-        withContext(Dispatchers.IO) { api.getMovie(id) }
+        withContext(Dispatchers.IO) {
+            if (map[id] == null) map[id] = api.getMovie(id)
+            map[id]!!
+        }
 
     override suspend fun getTrendingMovies(mediaType: String, timeWindow: String) =
-        withContext(Dispatchers.IO) { api.getTrending(mediaType, timeWindow) }
+        withContext(Dispatchers.IO) {
+            val movieResults = api.getTrending(mediaType, timeWindow)
+            for(movie: Movie in movieResults.results){
+                if(map[movie.id] == null) map[movie.id] = movie
+            }
+            movieResults
+        }
 
 }
