f39d56feaa0a9571940c219a0362c78339e84872
Steven Warlow <steven.warlow@mkodo.com>
2021-10-13 11:07:56 +0100

    [RM-85416] Changed initialization of view model in navigation.

--- app/src/main/java/com/mkodo/training/stevenw/movieapp/screens/movie/MovieNavigationFragment.kt
+++ app/src/main/java/com/mkodo/training/stevenw/movieapp/screens/movie/MovieNavigationFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import com.mkodo.training.stevenw.movieapp.R
 import com.mkodo.training.stevenw.movieapp.databinding.FragmentMovieNavigationBinding
@@ -19,7 +20,7 @@ class MovieNavigationFragment : Fragment(), TrendingCallback {
 
     private lateinit var trendingAdapter: TrendingAdapter
 
-    private val viewModel = MovieNavigationViewModel()
+    private val viewModel: MovieNavigationViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
